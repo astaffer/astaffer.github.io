24d72208704fe5a990693682d258d8161e87c045
Астафьев Павел Михайлович <astpm@volhovez.local>
2015-12-08 16:37:56 +0300

    working

--- js/mainApp.js
+++ js/mainApp.js
@@ -1,16 +1,22 @@
 var main = function(){
+	var active = $('#main_menu li.active');
+	var a_href = active.find('a').attr('href');
+		$.get("views\\"+a_href+".html", function(data){
+		$('.main').append(data);
+		});
 	$('#main_menu li').click(function(event){
 		event.preventDefault();
 		$('#main_menu li').removeClass('active');
 		$(this).toggleClass('active');
 		$('.main').empty();
-		var a_href = $(this).find('a').attr('href');
-		$.get("views\\"+a_href+".html", function(data){
+		var href = $(this).find('a').attr('href');
+		$.get("views\\"+href+".html", function(data){
 		$('.main').append(data);
 		});
-		var scope = angular.element($('.main')).scope();
-		scope.$apply();
+		//var scope = angular.element($('.main')).scope();
+		//scope.$apply();
 		//alert ("Href is: "+a_href);
 	});
+
 };
 $(document).ready(main);
