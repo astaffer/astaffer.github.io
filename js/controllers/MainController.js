53e6b7ca914e4140cbd6c364e950b614c8dece05
Астафьев Павел Михайлович <astpm@volhovez.local>
2015-12-11 08:55:08 +0300

    Added description

--- js/controllers/MainController.js
+++ js/controllers/MainController.js
@@ -5,30 +5,36 @@ app.controller('MainController', ['$scope', function($scope) {
 	    icon: 'img/butterlogo.svg', 
 	    title: 'МАСЛО', 
 	    color:'yellow',
+	    alt: 'Масло сливочное'
 	  },
 	  {
 	    icon: 'img/milklogo.svg', 
 	    title: 'МОЛОКО', 
 	    color:'white',
+alt: 'Молоко цельное'
 	  }, { 
 	    icon: 'img/cheeselogo.svg', 
 	    title: 'СЫР', 
 	    color:'yellow', 
+	alt: 'Сыр'
 	  }
 	, { 
 	    icon: 'img/yogurt.svg', 
 	    title: 'ЙОГУРТ', 
 	    color:'yellow', 
+alt: 'Йогурт'
 	  }
 	, { 
 	    icon: 'img/perishable.svg', 
 	    title: 'СКОРОПОРТ', 
 	    color:'yellow', 
+alt: 'Скоропорт'
 	  }
 	, { 
 	    icon: 'img/meltedcheese.svg', 
 	    title: 'ПЛАВЛЕННЫЙ СЫР', 
 	    color:'yellow', 
+alt: 'Плавленный сыр'
 	  }
 		, { 
 	    icon: 'img/other.svg', 
