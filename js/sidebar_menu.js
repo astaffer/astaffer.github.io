c3e7f29b238b2e9c8b6a64d0c81b02500c9f39c6
Astaffer <astaffer@gmail.com>
2016-07-13 16:12:54 +0300

    girls fix

--- js/sidebar_menu.js
+++ js/sidebar_menu.js
@@ -16,3 +16,35 @@ $(document).ready(function() {
 	 	});
 	 setTimeout(updateScrollSpy, 1000);
 });
+
+
+ function submitContactForm() {
+         /*var name = $('#form_name').val();
+  var email = $('#form_email').val();
+  var phone = $('#form_phone').val();
+  var message =$('#form_message').val();*/
+
+//this is the html template. You can also do it as used above. But is much simpler done as below
+
+ var htmlMessage = 'Contact form<br/>' +
+    'Name: '+$('#form_name').val()+'<br/>'+
+    'EMail: '+$('#form_email').val()+'<br/>'+
+    'Message<br/>'+
+    $('#form_message').val();
+
+//submit the form using ajax
+  $.ajax({
+  type: "POST",
+  url: "https://api:key-c94e48276bf7e5fe8995741b6cf936d9@api.mailgun.net/v3/sandboxab4f5d1ed492484ca80bc263745aea02.mailgun.org/messages",
+  data: {
+    "message": {
+      "from": 'Mailgun Sandbox <postmaster@sandboxab4f5d1ed492484ca80bc263745aea02.mailgun.org>',
+      "to": 'astaffer@gmail.com',
+      "subject": 'Hola',
+      "text": 'faq'
+    }
+  }
+});
+
+  return false;
+}
